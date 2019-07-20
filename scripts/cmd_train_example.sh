66031d4345962da83961d3862cf1a2de80dcf40d
Chengyu Huang <huangchengyu16@gmail.com>
2019-07-20 16:28:49 -0400

    add glue example

---
+++ scripts/cmd_train_example.sh
@@ -0,0 +1,14 @@
+python ./examples/run_glue.py \
+    --model_type bert \
+    --model_name_or_path bert-base-uncased \
+    --task_name MRPC \
+    --do_train \
+    --do_eval \
+    --do_lower_case \
+    --data_dir data/glue_data/MRPC \
+    --max_seq_length 128 \
+    --per_gpu_eval_batch_size=8   \
+    --per_gpu_train_batch_size=8   \
+    --learning_rate 2e-5 \
+    --num_train_epochs 3.0 \
+    --output_dir /tmp/MRPC/
