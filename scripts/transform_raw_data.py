133afadabf69d1461fae3284b307f6f6e9898275
Chengyu <huangchengyu16@gmail.com>
2019-07-21 18:06:17 -0400

    add training AIV

---
+++ scripts/transform_raw_data.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 21 15:39:22 2019
+
+@author: chengyu
+"""
+
+## transform raw data to tsv
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import os
+
+#%%
+
+if __name__ == "__main__":
+    raw_data = "/home/chengyu/Dev/Sentiment_Transformer/data/authority_views/Authorities Views 20190703_Yoko_Chengyu_Harry_Comp.xlsx"
+    
+    df = pd.read_excel(raw_data)
+    df = df[['paragraph','Yoko_new']]
+    df['filter'] = df['Yoko_new'].map(lambda x: x in [-1,1,0,999])
+    df= df[df['filter']][['paragraph','Yoko_new']]
+    arr = df.index.to_numpy()
+    out = np.random.permutation(arr) # random shuffle
+    df = df.loc[out]
+    
+    n_train = int(len(df)*0.7)
+    n_dev = int(len(df)*0.3)
+    n_test = len(df) - n_train - n_dev
+    
+    df_train = df[:n_train]
+    df_dev = df[n_train:n_train+n_dev]
+    df_test = df[-n_test:]
+    
+    ## export 
+    out_folder = "/home/chengyu/Dev/Sentiment_Transformer/data/authority_views"
+    
+    df_train.to_csv(os.path.join(out_folder,'train.tsv'),index=False,sep='\t')
+    df_dev.to_csv(os.path.join(out_folder,'dev.tsv'),index=False,sep='\t')
+    df_test.to_csv(os.path.join(out_folder,'test.tsv'),index=False,sep='\t')
+    
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#conda create -n pytorch_transformer python==3.7 
+#source activate pytorch_transformer
+
+conda install numpy pandas scipy scikit-learn 
+pip install pytorch-transformers
+pip install tensorboardX
