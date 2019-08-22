8489c56e673bb4d85ba01cdad9e2d6196e6e08a5
Chengyu <huangchengyu16@gmail.com>
2019-08-22 15:03:41 -0400

    inference authorities

--- scripts/transform_raw_data.py
+++ scripts/transform_raw_data.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     #raw_data = "/home/chengyu/Dev/Sentiment_Transformer/data/authority_views/Authorities Views 20190703_Yoko_Chengyu_Harry_Comp.xlsx"
     #raw_data = "../data/authority_views/Authorities Views_training_v2.xlsx"
     data_type = 'train'
-    
+    #data_type = 'authorities_views'
     raw_data = "../data/authority_views/{}.xlsx".format(data_type)
     
     df = pd.read_excel(raw_data)
